6ddcd3e69ba6a46f3c36950fffd1ccd733ed9f51
kimdaegwon <daegwonkim.dev@gmail.com>
2025-09-12 18:42:35 +0900

    test: Redis 동작 확인을 위한 간단한 테스트 코드 작성

--- src/test/java/com/example/redis_practice/RedisPracticeApplicationTests.java
+++ src/test/java/com/example/redis_practice/RedisPracticeApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.redis_practice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class RedisPracticeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
+    @Test
+    void testStrings() {
+        // given
+        ValueOperations<String, String> ops = redisTemplate.opsForValue();
+        String key = "key";
+
+        // when
+        ops.set(key, "value");
+
+        // then
+        String value = ops.get(key);
+        assertThat(value).isEqualTo("value");
+    }
 }
